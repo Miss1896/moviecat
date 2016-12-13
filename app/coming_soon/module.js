5ea49f792f106cb7283aedc15ba8a15d8eab2060
itcast <test@itcast.cn>
2016-12-13 17:53:12 +0800

    即将上映、top250分页功能完成

--- app/coming_soon/module.js
+++ app/coming_soon/module.js
@@ -9,17 +9,40 @@
             controller:'coming_soonController'
         })
 
-    }]).controller('coming_soonController',['$scope','$http',function($scope,$http){
+    }]).controller('coming_soonController',['$scope','$http','$routeParams','$route','itcastJSONP',function($scope,$http,$routeParams,$route,itcastJSONP){
 
-        $http({
-            method:'GET',
-            url:'./coming_soon/data.json'
-        }).then(function(response){
-            // console.log(response);
-            $scope.movie = response.data;
-        },function(){
+        // $http({
+        //     method:'GET',
+        //     url: './in_theaters/data.json'
+        // }).then(function(response){
+        //     //console.log(response);
+        //     $scope.movie = response.data;
 
-        })
+        // },function(){
+        //     // console.log(234);
+        // })
+        
+        $scope.pageSize = 5;
+        $scope.curPage = $routeParams.page || 1;
+        var start = ($scope.curPage - 1) * $scope.pageSize ;
+
+        itcastJSONP.jsonp('http://api.douban.com/v2/movie/coming_soon',
+            {start:start,count:$scope.pageSize},
+            function(data){
+                $scope.movie = data;
+                console.log(data);
+                $scope.totalPages = Math.ceil(data.total / $scope.pageSize);
+                $scope.$apply();
+        });
+        
+        $scope.goPages = function(current){
+            if(current <=0  || current > $scope.totalPages){
+                return false;
+            }
+            $route.updateParams({page:current});
+        }
+
     }])
+
 
 })(angular)
--- app/top250/module.js
+++ app/top250/module.js
@@ -10,18 +10,40 @@
             controller:'Top250Controller'
         })
 
-    }]).controller('Top250Controller',['$scope','$http',function($scope,$http){
+    }]).controller('Top250Controller',['$scope','$http','$routeParams','$route','itcastJSONP',function($scope,$http,$routeParams,$route,itcastJSONP){
 
-        $http({
-            method:'GET',
-            url:'./top250/data.json'
-        }).then(function(response){
-            // console.log(123);
-            $scope.movie = response.data;
-        },function(){
+        // $http({
+        //     method:'GET',
+        //     url: './in_theaters/data.json'
+        // }).then(function(response){
+        //     //console.log(response);
+        //     $scope.movie = response.data;
 
-        })
+        // },function(){
+        //     // console.log(234);
+        // })
+        
+        $scope.pageSize = 5;
+        $scope.curPage = $routeParams.page || 1;
+        var start = ($scope.curPage - 1) * $scope.pageSize ;
+
+        itcastJSONP.jsonp('http://api.douban.com/v2/movie/top250',
+            {start:start,count:$scope.pageSize},
+            function(data){
+                $scope.movie = data;
+                console.log(data);
+                $scope.totalPages = Math.ceil(data.total / $scope.pageSize);
+                $scope.$apply();
+        });
+        
+        $scope.goPages = function(current){
+            if(current <=0  || current > $scope.totalPages){
+                return false;
+            }
+            $route.updateParams({page:current});
+        }
 
     }])
+
 
 })(angular);
