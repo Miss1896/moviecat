8fd712bbd31634c417becacf072e441a38a584ae
itcast <test@itcast.cn>
2016-12-14 14:26:29 +0800

    首页完成

--- app/app.js
+++ app/app.js
@@ -3,12 +3,7 @@
 
     // start your ride
     angular.module('moviecat',[
-        'moviecat.home',
-        'moviecat.movielist',
-        // 'moviecat.in_theaters',
-        // 'moviecat.coming_soon',
-        // 'moviecatTop250',
-        'moviecat.jsonp'
+        'moviecat.home'       
     ]);
 
 })(angular);
