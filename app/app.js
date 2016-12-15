648063fe5edac8d48397a57ccec7cb5dfbf673f5
itcast <test@itcast.cn>
2016-12-15 12:44:29 +0800

    点击高亮显示效果完成

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@
     angular.module('moviecat',[
         'moviecat.home',
         'moviecat.movielist',
-        'moviecat.jsonp'    
+        'moviecat.jsonp',
+        'moviecat.autoActive'  
     ]);
 
 })(angular);
